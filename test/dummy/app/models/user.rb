e01ec02e8420c40f3c17958c8c0877c503c9aa67
Stephen Potenza <potenza@gmail.com>
2014-03-27 13:03:34 -0700

    fixing dummy model validation to work with a boolean column

--- test/dummy/app/models/user.rb
+++ test/dummy/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   serialize :preferences
 
   validates :email, presence: true, :length => { minimum: 5 }
-  validates :terms, acceptance: true
+  validates :terms, acceptance: { accept: true }
 
   has_one :address
   accepts_nested_attributes_for :address
