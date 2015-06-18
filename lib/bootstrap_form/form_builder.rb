f80d28a4f41a05899629d64585a4f013c5094fdc
Sean Naegle <scnaegle@gmail.com>
2015-06-18 17:09:34 -0600

    Don't remove the id so that when someone puts wrapper: {id: "something"} it works as expected

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -181,7 +181,7 @@ module BootstrapForm
       options[:class] << " #{error_class}" if has_error?(name)
       options[:class] << " #{feedback_class}" if options[:icon]
 
-      content_tag(:div, options.except(:id, :label, :help, :icon, :label_col, :control_col, :layout)) do
+      content_tag(:div, options.except(:label, :help, :icon, :label_col, :control_col, :layout)) do
         label = generate_label(options[:id], name, options[:label], options[:label_col], options[:layout]) if options[:label]
         control = capture(&block).to_s
         control.concat(generate_help(name, options[:help]).to_s)
