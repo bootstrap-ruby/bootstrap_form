7794a4b357765d4cfa9163020afc5554cab17260
Remus Rusanu <contact@rusanu.com>
2016-03-10 09:31:41 +0000

    add support for input-group-sm and input-group-lg in prepend/append

--- lib/bootstrap_form/helpers/bootstrap.rb
+++ lib/bootstrap_form/helpers/bootstrap.rb
@@ -60,12 +60,14 @@ module BootstrapForm
       end
 
       def prepend_and_append_input(options, &block)
-        options = options.extract!(:prepend, :append)
+        options = options.extract!(:prepend, :append, :input_group_class)
+        input_group_class = ["input-group", options[:input_group_class]].compact.join(' ')
+
         input = capture(&block)
 
         input = content_tag(:span, options[:prepend], class: input_group_class(options[:prepend])) + input if options[:prepend]
         input << content_tag(:span, options[:append], class: input_group_class(options[:append])) if options[:append]
-        input = content_tag(:div, input, class: "input-group") unless options.empty?
+        input = content_tag(:div, input, class: input_group_class) unless options.empty?
         input
       end
 
