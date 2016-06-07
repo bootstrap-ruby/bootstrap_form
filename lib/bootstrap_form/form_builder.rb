d7981d26a484b74a77dab640b92aa9e95cd80dbf
Koen Punt <koen@koenpunt.nl>
2016-06-07 11:44:49 +0200

    remove deprecated methods

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -171,16 +171,6 @@ module BootstrapForm
 
     alias_method_chain :collection_radio_buttons, :bootstrap
 
-    def check_boxes_collection(*args)
-      warn "'BootstrapForm#check_boxes_collection' is deprecated, use 'BootstrapForm#collection_check_boxes' instead"
-      collection_check_boxes(*args)
-    end
-
-    def radio_buttons_collection(*args)
-      warn "'BootstrapForm#radio_buttons_collection' is deprecated, use 'BootstrapForm#collection_radio_buttons' instead"
-      collection_radio_buttons(*args)
-    end
-
     def form_group(*args, &block)
       options = args.extract_options!
       name = args.first
