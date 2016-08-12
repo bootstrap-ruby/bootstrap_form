0f115417fdcc1380bd43439edb456a9f637135a4
Pieter Vanmeerbeek <pv@vasco.com>
2016-08-12 10:14:55 +0200

    Don't rely on Class#name for model naming
    taken from https://github.com/bootstrap-ruby/rails-bootstrap-forms/pull/217 (@boffbowsh)
    
    Relying on Class#name for model naming breaks compatibility with
    anonymous models. The ActiveModel::Naming#model_name method is
    preferred
    
    Also, don’t attempt to get help text for nil objects. This was
    previously attempting to lookup activerecord.help.nil_class.field_name or similar.

---
+++ test/special_form_class_models_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class SpecialFormClassModelsTest < ActionView::TestCase
+  include BootstrapForm::Helper
+
+  test "Anonymous models are supported for form builder" do
+    user_klass = Class.new(User)
+    def user_klass.model_name
+      ActiveModel::Name.new(User)
+    end
+
+    @user = user_klass.new(email: 'steve@example.com', password: 'secret', comments: 'my comment')
+    @builder = BootstrapForm::FormBuilder.new(:user, @user, self, {})
+    @horizontal_builder = BootstrapForm::FormBuilder.new(:user, @user, self, {layout: :horizontal, label_col: "col-sm-2", control_col: "col-sm-10"})
+    I18n.backend.store_translations(:en, {activerecord: {help: {user: {password: "A good password should be at least six characters long"}}}})
+
+    expected = %{<div class="form-group"><label class="control-label" for="user_misc">Misc</label><input class="form-control" id="user_misc" name="user[misc]" type="date" /></div>}
+    assert_equal expected, @builder.date_field(:misc)
+  end
+
+
+  test "Nil models are supported for form builder" do
+    @user = nil
+    @builder = BootstrapForm::FormBuilder.new(:user, @user, self, {})
+    @horizontal_builder = BootstrapForm::FormBuilder.new(:user, @user, self, {layout: :horizontal, label_col: "col-sm-2", control_col: "col-sm-10"})
+    I18n.backend.store_translations(:en, {activerecord: {help: {user: {password: "A good password should be at least six characters long"}}}})
+
+    expected = %{<div class="form-group"><label class="control-label" for="user_misc">Misc</label><input class="form-control" id="user_misc" name="user[misc]" type="date" /></div>}
+    assert_equal expected, @builder.date_field(:misc)
+  end
+
+end
