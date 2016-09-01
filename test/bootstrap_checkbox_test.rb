bootstrap-ruby/bootstrap_form

commit 9a846c989cc596a65dde8ad09f86b66091c4d590

Allow the label option to be passed in as a hash
When generating fields, it should be possible to specify a hash in the
format of `label: { text: "A label", class: "a-class" }`, as indicated
in the documentation.

This approach assumes that if the developer has specified the label as a
hash, then it trumps `label_class` in the options hash.

lib/bootstrap_form/form_builder.rb
@@ -309,11 +309,20 @@ module BootstrapForm
       end
 
       unless options.delete(:skip_label)
-        label_class = hide_class if options.delete(:hide_label)
+        if options[:label].is_a?(Hash)
+          label_text  = options[:label].delete(:text)
+          label_class = options[:label].delete(:class)
+          options.delete(:label)
+        end
         label_class ||= options.delete(:label_class)
+        label_class = hide_class if options.delete(:hide_label)
 
-        form_group_options.reverse_merge!(label: {
-          text: options.delete(:label),
+        if options[:label].is_a?(String)
+          label_text ||= options.delete(:label)
+        end
+
+        form_group_options.merge!(label: {
+          text: label_text,
           class: label_class
         })
       end

test/bootstrap_form_group_test.rb
@@ -7,9 +7,14 @@ class BootstrapFormGroupTest < ActionView::TestCase
     setup_test_fixture
   end
 
-  test "changing the label text" do
+  test "changing the label text via the label option parameter" do
     expected = %{<div class="form-group"><label class="control-label required" for="user_email">Email Address</label><input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
     assert_equal expected, @builder.text_field(:email, label: 'Email Address')
+  end
+
+  test "changing the label text via the html_options label hash" do
+    expected = %{<div class="form-group"><label class="control-label required" for="user_email">Email Address</label><input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
+    assert_equal expected, @builder.text_field(:email, label: {text: 'Email Address'})
   end
 
   test "hiding a label" do
@@ -17,9 +22,19 @@ class BootstrapFormGroupTest < ActionView::TestCase
     assert_equal expected, @builder.text_field(:email, hide_label: true)
   end
 
-  test "adding a custom label class" do
+  test "adding a custom label class via the label_class parameter" do
     expected = %{<div class="form-group"><label class="btn control-label required" for="user_email">Email</label><input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
     assert_equal expected, @builder.text_field(:email, label_class: 'btn')
+  end
+
+  test "adding a custom label class via the html_options label hash" do
+    expected = %{<div class="form-group"><label class="btn control-label required" for="user_email">Email</label><input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
+    assert_equal expected, @builder.text_field(:email, label: {class: 'btn'})
+  end
+
+  test "adding a custom label and changing the label text via the html_options label hash" do
+    expected = %{<div class="form-group"><label class="btn control-label required" for="user_email">Email Address</label><input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
+    assert_equal expected, @builder.text_field(:email, label: {class: 'btn', text: "Email Address"})
   end
 
   test "skipping a label" do

commit 39bf7833d544a6ccd41276d8f83e3cc0de3f5252

Fix `hide_label` option for `collection_check_boxes`
When `hide_label` and `label_class` options are given to
`collection_check_boxes`, `label_class` should be rendered to label of
each checkbox.

This fixes an unexpected change that caused by 9a846c98.

lib/bootstrap_form/form_builder.rb
@@ -334,13 +334,13 @@ module BootstrapForm
       end
 
       unless options.delete(:skip_label)
+        label_class = hide_class if options.delete(:hide_label)
         if options[:label].is_a?(Hash)
-          label_text  = options[:label].delete(:text)
-          label_class = options[:label].delete(:class)
+          label_text = options[:label].delete(:text)
+          label_class ||= options[:label].delete(:class)
           options.delete(:label)
         end
         label_class ||= options.delete(:label_class)
-        label_class = hide_class if options.delete(:hide_label)
 
         if options[:label].is_a?(String)
           label_text ||= options.delete(:label)

test/bootstrap_checkbox_test.rb
@@ -140,5 +140,11 @@ class BootstrapCheckboxTest < ActionView::TestCase
     assert_equal expected, @builder.collection_check_boxes(:misc, collection, lambda { |a| "address_#{a.id}" }, :street, checked: collection)
   end
 
+  test 'collection_check_boxes renders :label_class with :hide_label option' do
+    collection = [Address.new(id: 1, street: 'Foo'), Address.new(id: 2, street: 'Bar')]
+    expected = %{<input id="user_misc" multiple="multiple" name="user[misc][]" type="hidden" value="" /><div class="form-group"><label class="sr-only control-label" for="user_misc">Misc</label><div class="checkbox"><label class="my-label" for="user_misc_1"><input id="user_misc_1" name="user[misc][]" type="checkbox" value="1" /> Foo</label></div><div class="checkbox"><label class="my-label" for="user_misc_2"><input id="user_misc_2" name="user[misc][]" type="checkbox" value="2" /> Bar</label></div></div>}
+
+    assert_equal expected, @builder.collection_check_boxes(:misc, collection, :id, :street, hide_label: true, label_class: 'my-label')
+  end
 
 end