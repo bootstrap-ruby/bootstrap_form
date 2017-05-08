abevoelker/rails-bootstrap-forms

Rendering input with bootstrap but without wrapper

test/bootstrap_form_group_test.rb
@@ -245,6 +245,11 @@ class BootstrapFormGroupTest < ActionView::TestCase
     assert_equivalent_xml expected, @builder.search_field(:misc, wrapper: { data: { foo: 'bar' } })
   end
 
+  test "rendering without wrapper" do
+    expected = %{<input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" />}
+    assert_equal expected, @builder.text_field(:email, wrapper: false)
+  end
+
   test "passing options to a form control get passed through" do
     expected = %{<div class="form-group"><label class="form-control-label required" for="user_email">Email</label><input autofocus="autofocus" class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
     assert_equivalent_xml expected, @builder.text_field(:email, autofocus: true)

bootstrap-ruby/bootstrap_form

test/bootstrap_form_group_test.rb
@@ -245,6 +245,11 @@ class BootstrapFormGroupTest < ActionView::TestCase
     assert_equivalent_xml expected, @builder.search_field(:misc, wrapper: { data: { foo: 'bar' } })
   end
 
+  test "rendering without wrapper" do
+    expected = %{<input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" />}
+    assert_equivalent_xml expected, @builder.text_field(:email, wrapper: false)
+  end
+
   test "passing options to a form control get passed through" do
     expected = %{<div class="form-group"><label class="control-label required" for="user_email">Email</label><input autofocus="autofocus" class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
     assert_equivalent_xml expected, @builder.text_field(:email, autofocus: true)