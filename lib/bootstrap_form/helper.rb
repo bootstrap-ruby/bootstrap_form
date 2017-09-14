41e1c79b4f493d66f02ef7900d26f3af8fa40d1e
Larry Reid <lcreid@jadesystems.ca>
2017-09-14 08:46:51 -0700

    Make it parse under Ruby 1.9.2
    The `bootstrap_form_with` helper uses Ruby 2+ syntax. I put a test
    for the Rails version, so the helper is only parse if running
    Rails 5.1+.

--- lib/bootstrap_form/helper.rb
+++ lib/bootstrap_form/helper.rb
@@ -20,14 +20,16 @@ module BootstrapForm
       bootstrap_form_for('', options, &block)
     end
 
-    def bootstrap_form_with(model: nil, scope: nil, url: nil, format: nil, **options, &block)
-      options.reverse_merge!(builder: BootstrapForm::FormBuilderFormWith)
-      # options[:acts_like_form_tag] = true if model.nil?
+    if ::Rails::VERSION::STRING >= '5.1'
+      def bootstrap_form_with(model: nil, scope: nil, url: nil, format: nil, **options, &block)
+        options.reverse_merge!(builder: BootstrapForm::FormBuilderFormWith)
+        # options[:acts_like_form_tag] = true if model.nil?
 
-      options = process_options(options)
+        options = process_options(options)
 
-      temporarily_disable_field_error_proc do
-        form_with(model: model, scope: scope, url: url, format: format, **options, &block)
+        temporarily_disable_field_error_proc do
+          form_with(model: model, scope: scope, url: url, format: format, **options, &block)
+        end
       end
     end
 
