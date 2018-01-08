459576f00e73e1a1a99fd04bb4169ef0dc4747af
Oleg <oleg@khabarov.ca>
2018-01-08 15:15:44 -0800

    cleaning up test helper. removing methods from global scope

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,55 +4,58 @@ require 'nokogiri'
 require 'equivalent-xml'
 require 'mocha/mini_test'
 
-# Configure Rails Environment
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require_relative "./dummy/config/environment.rb"
 require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+class ActionView::TestCase
 
-def setup_test_fixture
-  @user = User.new(email: 'steve@example.com', password: 'secret', comments: 'my comment')
-  @builder = BootstrapForm::FormBuilder.new(:user, @user, self, {})
-  @horizontal_builder = BootstrapForm::FormBuilder.new(:user, @user, self, { layout: :horizontal, label_col: "col-sm-2", control_col: "col-sm-10" })
-  I18n.backend.store_translations(:en, {
-    activerecord: {
-      attributes: {
-        user: {
-          email: "Email"
-        }
-      },
-      help: {
-        user: {
-          password: "A good password should be at least six characters long"
+  def setup_test_fixture
+    @user = User.new(email: 'steve@example.com', password: 'secret', comments: 'my comment')
+    @builder = BootstrapForm::FormBuilder.new(:user, @user, self, {})
+    @horizontal_builder = BootstrapForm::FormBuilder.new(:user, @user, self, {
+      layout:       :horizontal,
+      label_col:    "col-sm-2",
+      control_col:  "col-sm-10"
+    })
+
+    I18n.backend.store_translations(:en, {
+      activerecord: {
+        attributes: {
+          user: {
+            email: "Email"
+          }
+        },
+        help: {
+          user: {
+            password: "A good password should be at least six characters long"
+          }
         }
       }
-    }
-  })
-end
-
-def sort_attributes doc
-  doc.dup.traverse do |node|
-    if node.is_a?(Nokogiri::XML::Element)
-      attributes = node.attribute_nodes.sort_by(&:name)
-      attributes.each do |attribute|
-        node.delete(attribute.name)
-        node[attribute.name] = attribute.value
-      end
-    end
-    node
+    })
   end
-end
 
-class ActionView::TestCase
+  def sort_attributes doc
+    doc.dup.traverse do |node|
+      if node.is_a?(Nokogiri::XML::Element)
+        attributes = node.attribute_nodes.sort_by(&:name)
+        attributes.each do |attribute|
+          node.delete(attribute.name)
+          node[attribute.name] = attribute.value
+        end
+      end
+      node
+    end
+  end
+
   def assert_equivalent_xml(expected, actual)
-    expected_xml = Nokogiri::XML(expected)
-    actual_xml = Nokogiri::XML(actual)
-    ignored_attributes = %w(style data-disable-with)
+    expected_xml        = Nokogiri::XML(expected)
+    actual_xml          = Nokogiri::XML(actual)
+    ignored_attributes  = %w(style data-disable-with)
+
     equivalent = EquivalentXml.equivalent?(expected_xml, actual_xml, {
       ignore_attr_values: ignored_attributes
     }) do |a, b, result|
@@ -75,6 +78,7 @@ class ActionView::TestCase
       end
       result
     end
+
     assert equivalent, lambda {
       # using a lambda because diffing is expensive
       Diffy::Diff.new(
@@ -83,4 +87,5 @@ class ActionView::TestCase
       ).to_s(:color)
     }
   end
+
 end
