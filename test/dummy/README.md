6388eee49dd0a4f4cd178a6b3e2196770603e9db
Oleg <oleg@khabarov.ca>
2018-01-08 16:28:20 -0800

    adding rails 5.2 dummy app. let's see if travis is still happy

---
+++ test/dummy/README.md
@@ -0,0 +1,7 @@
+# TEST DUMMY APP (Rails 5.2)
+
+Following files were added or changed:
+
+- db/schema.rb
+- config/application.rb
+- config/routes.rb
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,23 +1,25 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
+require "bootstrap_form"
 
 module Dummy
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+
+    if config.respond_to?(:load_defaults)
+      config.load_defaults [Rails::VERSION::MAJOR, Rails::VERSION::MINOR].join(".")
+    end
+
+    if config.respond_to?(:secret_key_base)
+      config.secret_key_base = "ignore"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
   end
 end
+
