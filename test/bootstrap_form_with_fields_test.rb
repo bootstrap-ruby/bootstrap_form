300c8cfa9bc4708fef7b8f0945586b6b00d4ddfa
Larry Reid <lcreid@jadesystems.ca>
2018-01-12 14:40:45 -0800

    Use heredoc.strip.

--- test/bootstrap_form_with_fields_test.rb
+++ test/bootstrap_form_with_fields_test.rb
@@ -1,3 +1,7 @@
+# The following allows us to test this file individually with:
+# 'bundle exec rake test TEST=test/bootstrap_form_with_fields_test.rb'
+require 'rails'
+
 if ::Rails::VERSION::STRING >= '5.1'
   require 'test_helper'
 
@@ -12,33 +16,75 @@ if ::Rails::VERSION::STRING >= '5.1'
     # potentially affected by the lack of default DOM ids from `form_with`.
 
     test "form_with text areas are wrapped correctly" do
-      expected = %{<div class="form-group"><label class="form-control-label">Comments</label><textarea class="form-control" name="user[comments]">\nmy comment</textarea></div>}
+      expected = <<-HTML.strip_heredoc
+        <div class="form-group">
+          <label class="form-control-label" for="user_comments">Comments</label>
+          <textarea class="form-control" id="user_comments" name="user[comments]">\nmy comment</textarea>
+        </div>
+      HTML
       # puts "Rails: #{ActionView::Helpers::FormBuilder.new(:user, @user, self, {}).text_area(:comments)}"
-      assert_equivalent_xml expected, @form_with_builder.text_area(:comments)
-      expected = %{<div class="form-group"><label class="form-control-label" for="user_comments">Comments</label><textarea class="form-control" id="user_comments" name="user[comments]">\nmy comment</textarea></div>}
-      # Doesn't the new Rails way mean everyone has to know how to generate Rails field IDs?
-      assert_equivalent_xml expected, @form_with_builder.text_area(:comments, id: :user_comments)
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), @form_with_builder.text_area(:comments)
+
+      expected = <<-HTML.strip_heredoc
+        <div class="form-group">
+          <label class="form-control-label" for="TEST_ID">Comments</label>
+          <textarea class="form-control" id="TEST_ID" name="user[comments]">\nmy comment</textarea>
+        </div>
+      HTML
+      assert_equivalent_xml expected, @form_with_builder.text_area(:comments, id: :TEST_ID)
     end
 
     test "form_with password fields are wrapped correctly" do
-      expected = %{<div class="form-group"><label class="form-control-label">Password</label><input class="form-control" name="user[password]" type="password" /><span class="form-text text-muted">A good password should be at least six characters long</span></div>}
-      assert_equivalent_xml expected, @form_with_builder.password_field(:password)
-      expected = %{<div class="form-group"><label class="form-control-label" for="user_password">Password</label><input class="form-control" id="user_password" name="user[password]" type="password" /><span class="form-text text-muted">A good password should be at least six characters long</span></div>}
-      assert_equivalent_xml expected, @form_with_builder.password_field(:password, id: :user_password)
+      expected = <<-HTML.strip_heredoc
+        <div class="form-group">
+          <label class="form-control-label" for="user_password">Password</label>
+          <input class="form-control" id="user_password" name="user[password]" type="password" />
+          <span class="form-text text-muted">A good password should be at least six characters long</span>
+        </div>
+      HTML
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), @form_with_builder.password_field(:password)
+      expected = <<-HTML.strip_heredoc
+        <div class="form-group">
+          <label class="form-control-label" for="TEST_ID">Password</label>
+          <input class="form-control" id="TEST_ID" name="user[password]" type="password" />
+          <span class="form-text text-muted">A good password should be at least six characters long</span>
+        </div>
+      HTML
+      assert_equivalent_xml expected, @form_with_builder.password_field(:password, id: :TEST_ID)
     end
 
     test "form_with file fields are wrapped correctly" do
-      expected = %{<div class="form-group"><label class="form-control-label">Misc</label><input name="user[misc]" type="file" /></div>}
-      assert_equivalent_xml expected, @form_with_builder.file_field(:misc)
-      expected = %{<div class="form-group"><label class="form-control-label" for="user_misc">Misc</label><input id="user_misc" name="user[misc]" type="file" /></div>}
-      assert_equivalent_xml expected, @form_with_builder.file_field(:misc, id: :user_misc)
+      expected = <<-HTML.strip_heredoc
+      <div class="form-group">
+        <label class="form-control-label" for="user_misc">Misc</label>
+        <input id="user_misc" name="user[misc]" type="file" />
+      </div>
+      HTML
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), @form_with_builder.file_field(:misc)
+      expected = <<-HTML.strip_heredoc
+        <div class="form-group">
+          <label class="form-control-label" for="TEST_ID">Misc</label>
+          <input id="TEST_ID" name="user[misc]" type="file" />
+        </div>
+      HTML
+      assert_equivalent_xml expected, @form_with_builder.file_field(:misc, id: :TEST_ID)
     end
 
     test "form_with text fields are wrapped correctly" do
-      expected = %{<div class="form-group"><label class="form-control-label required">Email</label><input class="form-control" name="user[email]" type="text" value="steve@example.com" /></div>}
-      assert_equivalent_xml expected, @form_with_builder.text_field(:email)
-      expected = %{<div class="form-group"><label class="form-control-label required" for="user_email">Email</label><input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" /></div>}
-      assert_equivalent_xml expected, @form_with_builder.text_field(:email, id: :user_email)
+      expected = <<-HTML.strip_heredoc
+      <div class="form-group">
+        <label class="form-control-label required" for="user_email">Email</label>
+        <input class="form-control" id="user_email" name="user[email]" type="text" value="steve@example.com" />
+      </div>
+      HTML
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), @form_with_builder.text_field(:email)
+      expected = <<-HTML.strip_heredoc
+        <div class="form-group">
+          <label class="form-control-label required" for="TEST_ID">Email</label>
+          <input class="form-control" id="TEST_ID" name="user[email]" type="text" value="steve@example.com" />
+        </div>
+      HTML
+      assert_equivalent_xml expected, @form_with_builder.text_field(:email, id: :TEST_ID)
     end
 
     test "bootstrap_form_with helper works for associations" do
@@ -51,23 +97,31 @@ if ::Rails::VERSION::STRING >= '5.1'
       end
 
       expected = <<-HTML.strip_heredoc
-        <form accept-charset="UTF-8" action="/users" method="post" role="form">
-          <input name="utf8" type="hidden" value="&#x2713;" />
-          <div class="form-group">
-            <label class="form-control-label">Street</label>
-            <input class="form-control" name="user[address_attributes][street]" type="text" value="123 Main Street" />
-          </div>
-        </form>
+      <form accept-charset="UTF-8" action="/users" method="post" role="form">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <div class="form-group">
+          <label class="form-control-label" for="user_address_attributes_street">Street</label>
+          <input class="form-control" id="user_address_attributes_street" name="user[address_attributes][street]" type="text" value="123 Main Street" />
+        </div>
+      </form>
       HTML
-      assert_equivalent_xml expected, output
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), output
 
       output = bootstrap_form_with(model: @user, local: true) do |f|
         f.fields :address do |af|
-          af.text_field(:street, id: :user_address_attributes_street)
+          af.text_field(:street, id: :TEST_ID)
         end
       end
 
-      expected = %{<form accept-charset="UTF-8" action="/users" method="post" role="form"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><div class="form-group"><label class="form-control-label" for="user_address_attributes_street">Street</label><input class="form-control" id="user_address_attributes_street" name="user[address_attributes][street]" type="text" value="123 Main Street" /></div></form>}
+      expected = <<-HTML.strip_heredoc
+        <form accept-charset="UTF-8" action="/users" method="post" role="form">
+          <input name="utf8" type="hidden" value="&#x2713;" />
+          <div class="form-group">
+            <label class="form-control-label" for="TEST_ID">Street</label>
+            <input class="form-control" id="TEST_ID" name="user[address_attributes][street]" type="text" value="123 Main Street" />
+          </div>
+        </form>
+      HTML
       assert_equivalent_xml expected, output
     end
 
@@ -80,16 +134,32 @@ if ::Rails::VERSION::STRING >= '5.1'
         end
       end
 
-      expected = %{<form accept-charset="UTF-8" action="/users" method="post" role="form"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><div class="form-group"><label class="form-control-label">Favorite color</label><input class="form-control" name="user[preferences][favorite_color]" type="text" value="cerulean" /></div></form>}
-      assert_equivalent_xml expected, output
+      expected = <<-HTML.strip_heredoc
+      <form accept-charset="UTF-8" action="/users" method="post" role="form">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <div class="form-group">
+          <label class="form-control-label" for="user_preferences_favorite_color">Favorite color</label>
+          <input class="form-control" id="user_preferences_favorite_color" name="user[preferences][favorite_color]" type="text" value="cerulean" />
+        </div>
+      </form>
+      HTML
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), output
 
       output = bootstrap_form_with(model: @user, local: true) do |f|
         f.fields :preferences do |builder|
-          builder.text_field :favorite_color, value: @user.preferences[:favorite_color], id: :user_preferences_favorite_color
+          builder.text_field :favorite_color, value: @user.preferences[:favorite_color], id: :TEST_ID
         end
       end
 
-      expected = %{<form accept-charset="UTF-8" action="/users" method="post" role="form"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><div class="form-group"><label class="form-control-label" for="user_preferences_favorite_color">Favorite color</label><input class="form-control" id="user_preferences_favorite_color" name="user[preferences][favorite_color]" type="text" value="cerulean" /></div></form>}
+      expected = <<-HTML.strip_heredoc
+        <form accept-charset="UTF-8" action="/users" method="post" role="form">
+          <input name="utf8" type="hidden" value="&#x2713;" />
+          <div class="form-group">
+            <label class="form-control-label" for="TEST_ID">Favorite color</label>
+            <input class="form-control" id="TEST_ID" name="user[preferences][favorite_color]" type="text" value="cerulean" />
+          </div>
+        </form>
+      HTML
       assert_equivalent_xml expected, output
     end
 
@@ -102,16 +172,36 @@ if ::Rails::VERSION::STRING >= '5.1'
         end
       end
 
-      expected = %{<form accept-charset="UTF-8" action="/users" method="post" role="form"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><div class="form-group row"><label class="form-control-label col-sm-2">Street</label><div class="col-sm-10"><input class="form-control" name="user[address_attributes][street]" type="text" value="123 Main Street" /></div></div></form>}
-      assert_equivalent_xml expected, output
+      expected = <<-HTML.strip_heredoc
+      <form accept-charset="UTF-8" action="/users" method="post" role="form">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <div class="form-group row">
+          <label class="form-control-label col-sm-2" for="user_address_attributes_street">Street</label>
+          <div class="col-sm-10">
+            <input class="form-control" id="user_address_attributes_street" name="user[address_attributes][street]" type="text" value="123 Main Street" />
+          </div>
+        </div>
+      </form>
+      HTML
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), output
 
       output = bootstrap_form_with(model: @user, layout: :horizontal, label_col: 'col-sm-2', control_col: 'col-sm-10', local: true) do |f|
         f.fields :address do |af|
-          af.text_field(:street, id: :user_address_attributes_street)
+          af.text_field(:street, id: :TEST_ID)
         end
       end
 
-      expected = %{<form accept-charset="UTF-8" action="/users" method="post" role="form"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><div class="form-group row"><label class="form-control-label col-sm-2" for="user_address_attributes_street">Street</label><div class="col-sm-10"><input class="form-control" id="user_address_attributes_street" name="user[address_attributes][street]" type="text" value="123 Main Street" /></div></div></form>}
+      expected = <<-HTML.strip_heredoc
+      <form accept-charset="UTF-8" action="/users" method="post" role="form">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <div class="form-group row">
+          <label class="form-control-label col-sm-2" for="TEST_ID">Street</label>
+          <div class="col-sm-10">
+            <input class="form-control" id="TEST_ID" name="user[address_attributes][street]" type="text" value="123 Main Street" />
+          </div>
+        </div>
+      </form>
+      HTML
       assert_equivalent_xml expected, output
     end
 
@@ -124,16 +214,32 @@ if ::Rails::VERSION::STRING >= '5.1'
         end
       end
 
-      expected = %{<form accept-charset="UTF-8" action="/users" class="form-inline" method="post" role="form"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><div class="form-group"><label class="form-control-label">Street</label><input class="form-control" name="user[address_attributes][street]" type="text" value="123 Main Street" /></div></form>}
-      assert_equivalent_xml expected, output
+      expected = <<-HTML.strip_heredoc
+      <form accept-charset="UTF-8" action="/users" class="form-inline" method="post" role="form">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <div class="form-group">
+          <label class="form-control-label" for="user_address_attributes_street">Street</label>
+          <input class="form-control" id="user_address_attributes_street" name="user[address_attributes][street]" type="text" value="123 Main Street" />
+        </div>
+      </form>
+      HTML
+      assert_equivalent_xml remove_default_ids_for_rails_5_1(expected), output
 
       output = bootstrap_form_with(model: @user, layout: :inline, local: true) do |f|
         f.fields :address do |af|
-          af.text_field(:street, id: :user_address_attributes_street)
+          af.text_field(:street, id: :TEST_ID)
         end
       end
 
-      expected = %{<form accept-charset="UTF-8" action="/users" class="form-inline" method="post" role="form"><div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="&#x2713;" /></div><div class="form-group"><label class="form-control-label" for="user_address_attributes_street">Street</label><input class="form-control" id="user_address_attributes_street" name="user[address_attributes][street]" type="text" value="123 Main Street" /></div></form>}
+      expected = <<-HTML.strip_heredoc
+      <form accept-charset="UTF-8" action="/users" class="form-inline" method="post" role="form">
+        <input name="utf8" type="hidden" value="&#x2713;" />
+        <div class="form-group">
+          <label class="form-control-label" for="TEST_ID">Street</label>
+          <input class="form-control" id="TEST_ID" name="user[address_attributes][street]" type="text" value="123 Main Street" />
+        </div>
+      </form>
+      HTML
       assert_equivalent_xml expected, output
     end
   end
