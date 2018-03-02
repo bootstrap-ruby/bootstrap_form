3183572e76055cff1c280c45a65512fe693287bf
Parker Young <parker@parkedonrails.com>
2018-03-01 18:23:22 -0600

    Issue #444 - Help text rendered in wrong place.
    The help text and error message are no longer rendered in the same
    place. The help text is now rendered in the `form-group` div instead
    of in the `input-group` div. Before we could render the error and help
    text together, however that has changed and they need to be separate.
    
    With this separation we need to check in the `generate_help` method to
    see if there are errors and if inline errors are being rendered; as we
    don't want to render the help text then.

--- lib/bootstrap_form/helpers/bootstrap.rb
+++ lib/bootstrap_form/helpers/bootstrap.rb
@@ -67,7 +67,6 @@ module BootstrapForm
       end
 
       def prepend_and_append_input(name, options, &block)
-        help = options[:help]
         options = options.extract!(:prepend, :append, :input_group_class)
         input_group_class = ["input-group", options[:input_group_class]].compact.join(' ')
 
@@ -75,7 +74,7 @@ module BootstrapForm
 
         input = content_tag(:div, input_group_content(options[:prepend]), class: 'input-group-prepend') + input if options[:prepend]
         input << content_tag(:div, input_group_content(options[:append]), class: 'input-group-append') if options[:append]
-        input << generate_help(name, help).to_s
+        input << generate_error(name)
         input = content_tag(:div, input, class: input_group_class) unless options.empty?
         input
       end
