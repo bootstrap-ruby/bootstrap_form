76e89c343fef94572693dd5b10d2a5bc2aca5b6a
Thimo Jansen <thimo@defrog.nl>
2021-11-22 12:49:19 +0100

    Fix unwanted skipping of test if Rails < 7
    - also restructures the test to make it a bit more readable

--- test/bootstrap_rich_text_area_test.rb
+++ test/bootstrap_rich_text_area_test.rb
@@ -9,28 +9,26 @@ if ::Rails::VERSION::STRING > "6"
     setup :setup_test_fixture
 
     test "rich text areas are wrapped correctly" do
-      with_stub_token do
+      if ::Rails::VERSION::STRING >= "7"
+        with_stub_token do
+          expected = <<~HTML
+            <div class="mb-3">
+              <label class="form-label" for="user_life_story">Life story</label>
+              <input autocomplete="off" type="hidden" name="user[life_story]" id="user_life_story_trix_input_user"/>
+              <trix-editor class="trix-content form-control" data-blob-url-template="http://test.host/rails/active_storage/blobs/redirect/:signed_id/:filename" data-direct-upload-attachment-name="ActionText::RichText#embeds" data-direct-upload-token="token" data-direct-upload-url="http://test.host/rails/active_storage/direct_uploads" id="user_life_story" input="user_life_story_trix_input_user"/>
+            </div>
+          HTML
+          assert_equivalent_xml expected, form_with_builder.rich_text_area(:life_story)
+        end
+      else
         expected = <<~HTML
           <div class="mb-3">
             <label class="form-label" for="user_life_story">Life story</label>
-            <input #{'autocomplete="off"' if ::Rails::VERSION::STRING >= '7'}
-              type="hidden" name="user[life_story]" id="user_life_story_trix_input_user"/>
-            #{trix_editor_html}
+            <input type="hidden" name="user[life_story]" id="user_life_story_trix_input_user"/>
+            <trix-editor id="user_life_story" data-blob-url-template="#{data_blob_url_template}" data-direct-upload-url="http://test.host/rails/active_storage/direct_uploads" input="user_life_story_trix_input_user" class="trix-content form-control" />
           </div>
         HTML
         assert_equivalent_xml expected, form_with_builder.rich_text_area(:life_story)
-      end
-    end
-
-    def trix_editor_html
-      if ::Rails::VERSION::STRING >= "7"
-        <<~HTML
-          <trix-editor class="trix-content form-control" data-blob-url-template="http://test.host/rails/active_storage/blobs/redirect/:signed_id/:filename" data-direct-upload-attachment-name="ActionText::RichText#embeds" data-direct-upload-token="token" data-direct-upload-url="http://test.host/rails/active_storage/direct_uploads" id="user_life_story" input="user_life_story_trix_input_user"/>
-        HTML
-      else
-        <<~HTML
-          <trix-editor id="user_life_story" data-blob-url-template="#{data_blob_url_template}" data-direct-upload-url="http://test.host/rails/active_storage/direct_uploads" input="user_life_story_trix_input_user" class="trix-content form-control" />
-        HTML
       end
     end
 
@@ -39,8 +37,6 @@ if ::Rails::VERSION::STRING > "6"
     end
 
     def with_stub_token(&block)
-      return if ::Rails::VERSION::STRING < "7"
-
       ActiveStorage::DirectUploadToken.stub(:generate_direct_upload_token, "token", &block)
     end
   end
