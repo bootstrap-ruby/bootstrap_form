1a15a424e0dca71ee3b8b344ef6e00b3196ad334
Larry Reid <lcreid@jadesystems.ca>
2022-01-09 15:55:12 -0800

    Rails 7 demo app

---
+++ demo/webpack.config.js
@@ -0,0 +1,20 @@
+const path    = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    application: "./app/javascript/application.js"
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[name].js.map",
+    path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+}
