e816a95a865b112beb86de46733840c42d7ade62
Uwe Kubosch <uwe@datek.no>
2022-04-23 08:24:02 +0200

    Add example images to README.md

--- demo/app/controllers/bootstrap_controller.rb
+++ demo/app/controllers/bootstrap_controller.rb
@@ -10,5 +10,15 @@ class BootstrapController < ApplicationController
     @user_with_error = User.new
     @user_with_error.errors.add(:email)
     @user_with_error.errors.add(:misc)
+
+    if (@erb = params[:erb])
+      unless @erb.start_with? "<%= bootstrap"
+        @erb.prepend "<%= bootstrap_form_with model: @user, layout: :horizontal, local: true do |f| %>\n"
+        @erb << "<% end %>"
+      end
+      @erb.prepend %{<div class="p-3">}
+      @erb << "</div>"
+      render inline: @erb, layout: "application"
+    end
   end
 end
--- demo/test/system/bootstrap_test.rb
+++ demo/test/system/bootstrap_test.rb
@@ -2,6 +2,7 @@ require "application_system_test_case"
 
 class BootstrapTest < ApplicationSystemTestCase
   setup { screenshot_section :bootstrap }
+
   test "visiting the index" do
     screenshot_group :index
     visit root_url
@@ -14,6 +15,24 @@ class BootstrapTest < ApplicationSystemTestCase
       sleep 0.5
       screenshot header.text.downcase.tr(" ", "_"), crop: bounds(example)
     end
+  end
+
+  test "readme examples" do
+    screenshot_group :readme
+
+    readme = File.read(File.expand_path("../../../README.md", __dir__))
+    readme_with_images = readme.gsub(/(?:!\[[^]]*\]\([^)]+\)\s*)?```erb\n(.*?)\n```\s*/m).with_index do |_, i|
+      erb = $1
+      visit root_path erb: erb
+      screenshot :example, crop: bounds(find('.p-3'))
+      <<~MD
+        ![Example #{i + 1}](demo/doc/screenshots/bootstrap/readme/#{'%02i' % i}_example.png "Example #{i + 1}")
+        ```erb
+        #{erb}
+        ```\n
+      MD
+    end
+    File.write(File.expand_path("../../../README.md", __dir__), readme_with_images)
   end
 
   private
