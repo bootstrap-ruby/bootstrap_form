fec914e3a8f3123fc564721788165a390d3a6dee
Uwe Kubosch <donv@users.noreply.github.com>
2022-04-24 22:22:12 +0200

    Add generated HTML for the examples in README.md (#636)
    Remove images for empty examples in README.md

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,4 +1,5 @@
 Dummy::Application.routes.draw do
+  get "fragment" => "bootstrap#fragment", as: :fragment
   resources :users
 
   root to: "bootstrap#form"
--- demo/test/system/bootstrap_test.rb
+++ demo/test/system/bootstrap_test.rb
@@ -17,22 +17,52 @@ class BootstrapTest < ApplicationSystemTestCase
     end
   end
 
+  HEADERS = "This generates|Which outputs|will be rendered as".freeze
+  REGEXP =
+    /(?:!\[[^\]]*\]\([^)]+\)\s*)?```erb\n(.*?)\n```\s*((?:#{HEADERS}).*?$)?\s*(```html\n(?:.*?)\n```\s*)?/mi
+    .freeze
   test "readme examples" do
     screenshot_group :readme
 
     readme = File.read(File.expand_path("../../../README.md", __dir__))
-    readme_with_images = readme.gsub(/(?:!\[[^]]*\]\([^)]+\)\s*)?```erb\n(.*?)\n```\s*/m).with_index do |_, i|
-      erb = $1
-      visit root_path erb: erb
-      screenshot :example, crop: bounds(find('.p-3'))
+    augmented_readme = readme.gsub(REGEXP).with_index do |_, i|
+      erb = Regexp.last_match(1)
+      header = Regexp.last_match(2)
+      unless /\A<%= bootstrap[^>]*>\n\s*...\s*<% end %>\z/.match? erb
+        visit fragment_path erb: erb
+        wrapper = find(".p-3")
+        screenshot :example, crop: bounds(wrapper)
+        html = wrapper["innerHTML"].strip.gsub("><", ">\n<")
+        doc = Nokogiri::HTML.fragment(html)
+        doc.traverse do |node|
+          if node.is_a?(Nokogiri::XML::Element)
+            node.attributes.sort_by(&:first).each do |name, value|
+              node.delete(name)
+              node[name] = value
+            end
+          end
+        end
+        html = doc.to_html
+        image = <<~MD
+          ![Example #{i}](demo/doc/screenshots/bootstrap/readme/#{format('%02i', i)}_example.png "Example #{i}")
+        MD
+        html = <<~MD
+
+          #{header || 'This generates:'}
+
+          ```html
+          #{HtmlBeautifier.beautify(html)}
+          ```
+        MD
+      end
       <<~MD
-        ![Example #{i + 1}](demo/doc/screenshots/bootstrap/readme/#{'%02i' % i}_example.png "Example #{i + 1}")
-        ```erb
+        #{image}```erb
         #{erb}
-        ```\n
+        ```
+        #{html}
       MD
     end
-    File.write(File.expand_path("../../../README.md", __dir__), readme_with_images)
+    File.write(File.expand_path("../../../README.md", __dir__), augmented_readme)
   end
 
   private
