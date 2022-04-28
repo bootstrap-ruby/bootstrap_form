4f06e7662e9934fe1e1d22d16f8890e54aec959b
Uwe Kubosch <uwe@datek.no>
2022-04-28 14:57:42 +0200

    Add a couple of skills to the example data

--- demo/app/models/skill.rb
+++ demo/app/models/skill.rb
@@ -1,5 +1,15 @@
 class Skill
+  attr_accessor :id, :name
+
+  def initialize(id:, name:)
+    @id = id
+    @name = name
+  end
+
   def self.all
-    []
+    [
+      Skill.new(id: 1, name: 'Mind reading'),
+      Skill.new(id: 2, name: 'Farming'),
+    ]
   end
 end
