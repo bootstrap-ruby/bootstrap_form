b07a6520815e02d0f0d6dc1a8b5e2dbcafe7c7e6
Uwe Kubosch <uwe@kubosch.no>
2023-06-17 22:11:37 +0200

    Silence puma startup message during test

--- demo/test/application_system_test_case.rb
+++ demo/test/application_system_test_case.rb
@@ -2,8 +2,9 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Capybara::Screenshot::Diff
-  Capybara::Screenshot.enabled = ENV["CI"].blank?
   driven_by :selenium, using: :headless_chrome, screen_size: [960, 720] do |capabilities|
     capabilities.add_argument "force-device-scale-factor=1"
   end
+  Capybara::Screenshot.enabled = ENV["CI"].blank?
+  Capybara.server = :puma, { Silent: true }
 end
