0cce9ae71ad16edd4a626d50e67d16febba8ad33
Larry Reid <lcreid@jadesystems.ca>
2023-10-18 20:38:18 -0700

    RuboCop fixes

--- demo/config/environments/production.rb
+++ demo/config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
                                        .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Info include generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
