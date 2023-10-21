e78c06f6fef13d9aa01c89a934ccf1c5f7766bbd
Larry Reid <lcreid@jadesystems.ca>
2023-10-21 09:52:36 -0700

    docker-compose for Selenium tests

--- demo/test/application_system_test_case.rb
+++ demo/test/application_system_test_case.rb
@@ -2,9 +2,29 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Capybara::Screenshot::Diff
-  driven_by :selenium, using: :headless_chrome, screen_size: [960, 720] do |capabilities|
+
+  class << self
+    def remote_selenium? = @remote_selenium ||= ENV["SELENIUM_HOST"].present? || ENV["SELENIUM_PORT"].present?
+  end
+
+  options = if remote_selenium?
+              {
+                browser: :remote,
+                url: "http://#{ENV.fetch('SELENIUM_HOST', 'selenium')}:#{ENV.fetch('SELENIUM_PORT', '4444')}"
+              }
+            else
+              {}
+            end
+
+  driven_by :selenium, using: :headless_chrome, screen_size: [960, 720], options: options do |capabilities|
     capabilities.add_argument "force-device-scale-factor=1"
   end
+
   Capybara::Screenshot.enabled = ENV["CI"].blank?
   Capybara.server = :puma, { Silent: true }
+
+  if remote_selenium?
+    Capybara.server_host = "0.0.0.0"
+    Capybara.app_host = "http://#{ENV.fetch('TEST_APP_HOST', 'web')}:#{ENV.fetch('TEST_APP_PORT', Capybara.server_port)}"
+  end
 end
