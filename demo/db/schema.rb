e806d053d67e606d7f99c96071fa7c4a4d7cedf2
Larry Reid <lcreid@jadesystems.ca>
2023-10-28 10:41:51 -0700

    Add action_mailbox_inbound_emails to schema

--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -11,6 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20_220_109_230_956) do
+  create_table "action_mailbox_inbound_emails", force: :cascade do |t|
+    t.integer "status", default: 0, null: false
+    t.string "message_id", null: false
+    t.string "message_checksum", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["message_id", "message_checksum"], name: "index_action_mailbox_inbound_emails_uniqueness", unique: true
+  end
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
