a4591370135d7bd11eb30a25f136ead3bb53cf94
Larry Reid <lcreid@jadesystems.ca>
2024-10-13 23:34:47 +0000

    I'm getting pretty much to the end of my rope

--- demo/db/schema.rb
+++ demo/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2024_10_13_183912) do
+ActiveRecord::Schema.define(version: 2024_10_13_183912) do
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.integer "status", default: 0, null: false
     t.string "message_id", null: false
