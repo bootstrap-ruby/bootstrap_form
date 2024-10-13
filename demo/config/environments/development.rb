c3fd3b3e90fe26606d584b6a987f5c97a18959c1
Larry Reid <lcreid@jadesystems.ca>
2024-10-13 20:56:20 +0000

    Don't use config open that doesn't exist in some versions

--- demo/config/environments/development.rb
+++ demo/config/environments/development.rb
@@ -72,5 +72,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_controller.raise_on_missing_callback_actions = true
+  # config.action_controller.raise_on_missing_callback_actions = true
 end
