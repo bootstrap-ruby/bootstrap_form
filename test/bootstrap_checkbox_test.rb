1ef42f4e9cc3892f4b0701f78926f197cbc612c1
Larry Reid <lcreid@jadesystems.ca>
2025-06-27 23:09:24 +0000

    `collection_checkboxes`

--- test/bootstrap_checkbox_test.rb
+++ test/bootstrap_checkbox_test.rb
@@ -184,6 +184,26 @@ class BootstrapCheckboxTest < ActionView::TestCase
                                                                      label: "This is a checkbox collection", help: "With a help!")
   end
 
+  if Rails::VERSION::MAJOR >= 8
+    test "collection_checkboxes renders the form_group correctly" do
+      collection = [Address.new(id: 1, street: "Foobar")]
+      expected = <<~HTML
+        <input #{autocomplete_attr} id="user_misc" name="user[misc][]" type="hidden" value="" />
+        <div class="mb-3">
+          <label class="form-label" for="user_misc">This is a checkbox collection</label>
+          <div class="form-check">
+            <input class="form-check-input" id="user_misc_1" name="user[misc][]" type="checkbox" value="1" />
+            <label class="form-check-label" for="user_misc_1">Foobar</label>
+          </div>
+          <small class="form-text text-muted">With a help!</small>
+        </div>
+      HTML
+
+      assert_equivalent_html expected, @builder.collection_checkboxes(:misc, collection, :id, :street,
+                                                                      label: "This is a checkbox collection", help: "With a help!")
+    end
+  end
+
   test "collection_check_boxes renders multiple checkboxes correctly" do
     collection = [Address.new(id: 1, street: "Foo"), Address.new(id: 2, street: "Bar")]
     expected = <<~HTML
