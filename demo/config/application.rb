8f65c22f1eba107de469eace5da2ba4111e5bc7c
Larry Reid <lcreid@jadesystems.ca>
2025-10-23 21:49:11 +0000

    Upgrade to 8.1

--- demo/config/application.rb
+++ demo/config/application.rb
@@ -14,6 +14,7 @@ module Dummy
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
+    # TODO: I think `autoload_lib` was introduced in 7.1, so it can soon be removed.
     config.autoload_lib(ignore: %w[assets tasks]) if config.respond_to?(:autoload_lib)
 
     # Configuration for the application, engines, and railties goes here.
