d6bf9d1e1b32293a5d7f7686370c5006242e4ff4
Larry Reid <lcreid@jadesystems.ca>
2025-11-19 22:35:12 +0000

    Tweaks to demo/config/boot.rb after rebase

--- demo/config/boot.rb
+++ demo/config/boot.rb
@@ -1,6 +1,8 @@
-ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+# Set up gems listed in the Gemfile.
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../../Gemfile", __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bundler/setup" if File.exist?(ENV["BUNDLE_GEMFILE"])
+$LOAD_PATH.unshift File.expand_path("../../lib", __dir__)
 # Only run the following line if we bootsnap is loaded, because we don't load it for the individual tests.
 # The individual tests are the ones in `..`.
 # They do load this file, for reasons.
